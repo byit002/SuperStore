4cef3372d0b436a8e711c9b5da192c0bba2f08ac
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2023-12-27 13:16:00 -0600

    Fix: warning due to eslint error

--- App/index.tsx
+++ App/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { View } from 'react-native';
-import FirstScreenTest from './screens/FirstScreenTest'
+import FirstScreenTest from './screens/FirstScreenTest';
 
 function index() {
   return <FirstScreenTest/>;
